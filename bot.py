267125b084bf5b19404bd0015c3e4acad74e5760
TalgatKalbayev <149049654+TalgatKalbayev@users.noreply.github.com>
2026-04-30 16:37:50 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from datetime import datetime, date
 import json
 import os
 
-TOKEN = "8421225379:AAHL1feuSSm8PYAND7_NZWF6ksFvOJF8E44"
+import os
+TOKEN = os.environ.get("8421225379:AAHL1feuSSm8PYAND7_NZWF6ksFvOJF8E44")
 CHAT_ID = 752586577
 START_DATE = date(2026, 4, 4)
 START_AMOUNT = 940
